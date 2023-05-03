a50645cc7bc8a739b4bdd907bf5693eeac26aef0
Alan Hwader Chu <alanhdchu@AlanM1.local>
2023-05-03 15:58:20 -0700

    updated footer with materilaze

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,34 @@
 import { SocialIcon } from 'react-social-icons';
 
 function Footer() {
-    return <footer>
-        <SocialIcon url="https://www.linkedin.com/in/alan-hwader-chu/" />
-        <SocialIcon url="https://github.com/AlanChu61" />
-        <span>
-            Copyright &copy;{new Date().getFullYear()} Alan Hwader Chu. All rights reserved.
-        </span>
+    return <footer className="page-footer">
+        <div className="container">
+            <div className="row">
+                <div className="col l6 s12">
+                    <h5 className="white-text">Footer Content</h5>
+                    <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                </div>
+                <div className="col l4 offset-l2 s12">
+                    <h5 className="white-text">Links</h5>
+                    <ul>
+                        <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div className="footer-copyright">
+            <div className="container">
+                Copyright &copy;{new Date().getFullYear()} Alan Hwader Chu. All rights reserved.
+                <a className="grey-text text-lighten-4 right" href="#!"><SocialIcon url="https://www.linkedin.com/in/alan-hwader-chu/" />   <SocialIcon url="https://github.com/AlanChu61" /></a>
+            </div>
+        </div>
     </footer>
+
+
+
 
 }
 export default Footer;
