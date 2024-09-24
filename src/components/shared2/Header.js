246e117856ae86d6abf7edc231e1bfd99335d8f4
Alan Chu <alanhdchu@utexas.edu>
2024-09-23 22:02:49 -0500

    fixing and renaming

---
+++ src/components/shared2/Header.js
@@ -0,0 +1,25 @@
+import Nav from './Nav'
+import TextSlider from '../main/TextSlider'
+function Header() {
+    const textList = [
+        "Web Developer",
+        "Software Engineer",
+        "Front End Developer",
+        "Back End Developer",
+        "Full Stack Developer",
+        "Lifelong Learner",
+        "Problem Solver",
+        "Pokemon Enthusiast",
+        "Explorer of Life's Possibilities"
+
+    ];
+    const interval = 1000;
+    return (
+        <header className='Header'>
+            <Nav />
+            <TextSlider textList={textList} interval={interval} />
+        </header>
+    );
+}
+
+export default Header;
