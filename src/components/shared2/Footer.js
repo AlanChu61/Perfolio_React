8db96cc489035c5ee515b437c80ff031ceee32e2
Alan Chu <alanhdchu@utexas.edu>
2024-09-23 22:04:23 -0500

    fixing and renaming

--- src/components/shared2/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return (
-        <footer className="page-footer bg-success-subtle">
-            Copyright&copy; {new Date().getFullYear()} Alan Hwader Chu. All rights reserved.
-        </footer>
-    )
-}
-export default Footer;
